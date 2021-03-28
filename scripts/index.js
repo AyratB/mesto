1487fad3ebd29c852d7e8c0b938513115d56e68e
Ayrat Burnashev <airatbyr@yandex.ru>
2021-03-28 21:10:01 +0300

    feat: init cardsList

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,51 @@ let popupName = document.querySelector('#popup-name');
 let popupDescription = document.querySelector('#popup-description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
+let cardList = document.querySelector('.cards__list'); 
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+const addCardToList = function(cardData){
+    const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
+    const cardItem = cardTemplate.cloneNode(true);
+
+    const cardImage = cardItem.querySelector('.card__image');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    const cardDescription = cardItem.querySelector('.card__description');
+    cardDescription.textContent = cardData.name;
+
+    return cardItem;
+}
+
+//инициализация блока с карточками   
+initialCards.forEach(item => cardList.append(addCardToList(item)));
 
 function closePopup(){
     formElement.classList.remove('popup_opened');
